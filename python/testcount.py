9178b514afd87d70c91f0852e6db70cf066fbd3b
rsmith-github <rsmith.git@gmail.com>
2023-09-26 16:52:50 +0100

    Red case for count function

---
+++ python/testcount.py
@@ -0,0 +1,33 @@
+import unittest
+
+
+def count(s):
+    
+    result_object = {}
+    
+    # populate object based on string
+        
+    return result_object
+
+
+class TestCount(unittest.TestCase):
+    
+    def setUp(self):
+        self.input_1 = "abc"
+        self.input_2 = "aabbcc"
+
+    def test_basic_case(self):
+        self.assertEquals({'a': 1, 'b': 1, 'c': 1}, count("abc"))
+
+    def test_doubles(self):
+        self.assertEquals({'a': 2, 'b': 2, 'c': 2}, count("aabbcc"))
+
+    def test_doubles_and_one_single(self):
+        self.assertEquals({'a': 2, 'b': 2, 'c': 1}, count("aabbc"))
+        
+    def test_empty(self):
+        self.assertEqual({}, count(''))
+
+
+if __name__ == '__main__':
+    unittest.main()
