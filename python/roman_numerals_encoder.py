349c8fe862f1596adacf591008653fdab466282c
rsmith-github <rsmith.git@gmail.com>
2023-09-28 20:52:03 +0100

    add test cases

--- python/roman_numerals_encoder.py
+++ python/roman_numerals_encoder.py
@@ -4,4 +4,11 @@ def solution(n):
     return
 
 class TestSolution(unittest.TestCase):
-    pass
+    
+    def test_given_examples(self):    
+        self.assertEqual(solution(1),'I', "solution(1),'I'")
+        self.assertEqual(solution(4),'IV', "solution(4),'IV'")
+        self.assertEqual(solution(6),'VI', "solution(6),'VI'")
+        self.assertEqual(solution(14),'XIV', "solution(14),'XIV")
+        self.assertEqual(solution(21),'XXI', "solution(21),'XXI'")
+        self.assertEqual(solution(89),'LXXXIX', "solution(89),'LXXXIX'")
