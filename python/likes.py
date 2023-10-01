6e9ac19b4e8b1090fab561d67e36c54812c19b24
rsmith-github <rsmith.git@gmail.com>
2023-10-01 19:58:43 +0100

    Basic cases tests

---
+++ python/likes.py
@@ -0,0 +1,17 @@
+import unittest
+
+
+
+class TestLikes(unittest.TestCase):
+
+    def test_example_cases(self):
+        self.assertEqual(likes([]), 'no one likes this')
+        self.assertEqual(likes(['Peter']), 'Peter likes this')
+        self.assertEqual(likes(['Jacob', 'Alex']), 'Jacob and Alex like this')
+        self.assertEqual(likes(['Max', 'John', 'Mark']), 'Max, John and Mark like this')
+        self.assertEqual(likes(['Alex', 'Jacob', 'Mark', 'Max']), 'Alex, Jacob and 2 others like this')
+        
+        
+
+if __name__ == '__main__':
+    unittest.main()
