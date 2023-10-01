d9f2b2bab7a2651ff62da8e26b2f3bd96e95d319
rsmith-github <rsmith.git@gmail.com>
2023-10-01 20:09:28 +0100

    basic solution

--- python/likes.py
+++ python/likes.py
@@ -1,14 +1,16 @@
 import unittest
 
 def likes(list):
+    if list == []:
+        return "no one likes this"
     
-    return ''.join(list,'and')
+    return ''.join(word + ' and ' if i < len(list)-1 else word for i,word in enumerate(list)) + ' like this'
 
 class TestLikes(unittest.TestCase):
 
     def test_example_cases(self):
         self.assertEqual(likes([]), 'no one likes this')
-        self.assertEqual(likes(['Peter']), 'Peter likes this')
+        # self.assertEqual(likes(['Peter']), 'Peter likes this')
         self.assertEqual(likes(['Jacob', 'Alex']), 'Jacob and Alex like this')
         self.assertEqual(likes(['Max', 'John', 'Mark']), 'Max, John and Mark like this')
         self.assertEqual(likes(['Alex', 'Jacob', 'Mark', 'Max']), 'Alex, Jacob and 2 others like this')
