8dc684df90d2d67a3ecd939ceeeeec175b9c7554
rsmith-github <rsmith.git@gmail.com>
2024-04-23 14:03:29 +0100

    test cases for mergeAlternately

--- python/merge_strings_alternately.py
+++ python/merge_strings_alternately.py
@@ -1,7 +1,17 @@
 import unittest
 
+
 def mergeAlternately(word1, word2):
+
     return ""
 
 
+class TestMergeAlternately(unittest.TestCase):
 
+    def test_merge_alternately(self):
+        self.assertEqual(mergeAlternately('abc', 'def'), 'adbecf')
+        self.assertEqual(mergeAlternately('abc', 'pqr'), 'apbqcr')
+
+
+if __name__ == "__main__":
+    unittest.main()
