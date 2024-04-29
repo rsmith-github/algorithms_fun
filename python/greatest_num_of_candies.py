ce378185903577c9dafb9076e92fa5dae8476c6e
rsmith-github <rsmith.git@gmail.com>
2024-04-29 14:38:12 +0100

    make more succinct

--- python/greatest_num_of_candies.py
+++ python/greatest_num_of_candies.py
@@ -8,20 +8,17 @@ def kidsWithCandies(candies, extraCandies):
     :rtype: List[bool]
     """
     big = max(candies)
-    res = []
-    for i in candies:
-        if i + extraCandies >= big:
-            res.append(True)
-        else:
-            res.append(False)
-    return res
+
+    return [i + extraCandies >= big for i in candies]
 
 
 class TestKidsWithCandies(unittest.TestCase):
 
     def test_kids_with_candies(self):
-        self.assertEqual(kidsWithCandies([2, 3, 5, 1, 3], 3), [True, True, True, False, True])
-        self.assertEqual(kidsWithCandies([4,2,1,1,2], 1), [True,False,False,False,False])
+        self.assertEqual(kidsWithCandies([2, 3, 5, 1, 3], 3), [
+                         True, True, True, False, True])
+        self.assertEqual(kidsWithCandies([4, 2, 1, 1, 2], 1), [
+                         True, False, False, False, False])
 
 
 if __name__ == "__main__":
