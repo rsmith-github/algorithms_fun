43d97a9e0c0d9c443b000eb3eca4fefb0310ac33
rsmith-github <rsmith.git@gmail.com>
2024-06-06 04:48:14 +0100

    red cases for can_place_flowers

---
+++ python/can-place-flowers.py
@@ -0,0 +1,22 @@
+import unittest
+
+
+def can_place_flowers(flowerbed, n):
+    
+    
+    
+    pass
+    
+    
+
+class TestCanPlaceFlowers(unittest.TestCase):
+    
+    def test_can_place_flowers(self):
+        
+        self.assertEqual(can_place_flowers(), True)
+        self.assertEqual(can_place_flowers(), False)
+        
+
+
+if __name__ == "__main__":
+    unittest.main()
