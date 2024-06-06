79cf256b7ec7fa83d743556b1d1c34f3be9b2ecf
rsmith-github <rsmith.git@gmail.com>
2024-06-06 17:49:35 +0100

    two pointer method

--- python/reverse-vowels-of-a-string.py
+++ python/reverse-vowels-of-a-string.py
@@ -2,21 +2,33 @@ import unittest
 
 
 def reverseVowels(s: str) -> str:
+
+    vowels = "aeiouAEIOU"
+    s_as_list = list(s)
+    left = 0
+    right = len(s) - 1
     
-    pass
+    while left < right:
+        
+        if s_as_list[left] not in vowels:
+            left += 1
+        if s_as_list[right] not in vowels:
+            right -= 1
+            
+        if s_as_list[left] in vowels and s_as_list[right] in vowels:
+            s_as_list[left], s_as_list[right] = s_as_list[right], s_as_list[left]
+            left += 1
+            right -= 1
+        
+    return "".join(s_as_list)
 
 
 class TestReverseVowels(unittest.TestCase):
-    
-    
+
     def test_reverse_vowels(self):
-        
+
         self.assertEqual(reverseVowels('hello'), 'holle')
         self.assertEqual(reverseVowels('leetcode'), 'leotcede')
-        
-        
-        
-
 
 
 if __name__ == "__main__":
