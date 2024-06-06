9e37de2006872861f023a48f7b086bacc1ad8160
rsmith-github <rsmith.git@gmail.com>
2024-06-06 05:19:55 +0100

    add solution

--- python/can-place-flowers.py
+++ python/can-place-flowers.py
@@ -4,17 +4,32 @@ import unittest
 def can_place_flowers(flowerbed, n):
     
     
+    for i in range(len(flowerbed)):
+        # last element
+        if i == len(flowerbed) - 1 and flowerbed[i] == 0 and flowerbed[i-1] == 0:
+            n -= 1
+        elif ( i == 0 and flowerbed[i] == 0 and flowerbed[i+1] == 0 ) or ( flowerbed[i] == 0 and flowerbed[i-1] == 0 and flowerbed[i+1] == 0 ): # first element or in between
+            n -= 1
+            flowerbed[i] = 1 
+
+    return n <= 0
+
     
-    pass
-    
-    
+
+
+
 
 class TestCanPlaceFlowers(unittest.TestCase):
     
     def test_can_place_flowers(self):
         
-        self.assertEqual(can_place_flowers(), True)
-        self.assertEqual(can_place_flowers(), False)
+        self.assertEqual(can_place_flowers([1,0,0,0,1], 1), True)
+        self.assertEqual(can_place_flowers([1,0,0,0,1], 2), False)
+        self.assertEqual(can_place_flowers([0,0,1,0,1], 1), True)
+        self.assertEqual(can_place_flowers([0,0,1,0,1,0,0,1,0,1], 1), True)
+        self.assertEqual(can_place_flowers([0,0,1,0,1,0,0,1,0,1], 2), False)
+        self.assertEqual(can_place_flowers([0,0,1,0,1,0,0,1,0,1], 3), False)
+        
         
 
 
