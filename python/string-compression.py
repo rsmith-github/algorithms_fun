5f0a37959ba57cca05d3aef26fbae5f0f2feba8f
rsmith-github <rsmith.git@gmail.com>
2024-06-12 11:22:18 +0100

    solution

--- python/string-compression.py
+++ python/string-compression.py
@@ -1,8 +1,51 @@
 import unittest
 
 def compress(chars):
+    # starting variables
+    s = ''
+    count = 0
+    prev_char = chars[0] # previous character is the first character
 
-    pass
+    # loop through input array, comparing each character to previous character.
+    for i, current_char in enumerate(chars):
+
+        # if previous is equal to current character, count it.
+        if prev_char == current_char:
+            count += 1
+        else: # else add previous character string
+            s += prev_char
+            
+            # and if count is not 1, add the count as well, according to the expected output.
+            if count != 1:
+                s += str(count)
+
+            # set previous character to current character
+            prev_char = current_char
+            
+            # set count to 1, because there is now 1 new character.
+            count = 1
+        
+        # if last at last character,
+        if i == len(chars) - 1:
+
+            # make sure to add it to the 'empty' string
+            s += current_char
+            
+            # again, if count is not 1, add it. (constraint in problem description)
+            if count != 1:
+                s += str(count)
+    
+    # modify the input list
+    for i, ch in enumerate(list(s)):
+        chars[i] = ch
+
+    # remove all the extra elements
+    while len(chars) != len(s):
+        chars.pop()
+
+    # return length of modified array
+    return len(chars)
+
 
 
 class TestCompress(unittest.TestCase):
