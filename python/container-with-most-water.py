bb1cec2b133f9be2e2a5e57aeaf606fe45a09346
rsmith-github <rsmith.git@gmail.com>
2024-06-13 18:23:28 +0100

    pseudocode

--- python/container-with-most-water.py
+++ python/container-with-most-water.py
@@ -3,6 +3,19 @@ import unittest
 
 def container_with_most_water(list):
     
+    
+    # keep track of current max area
+    
+    
+    # two pointer while loop
+    
+        # if left bar is lower or equal to right bar, calculate area and move left pointer
+        
+        
+        # if right bar is lower or equal to left bar, calculate area and move right pointer
+        
+    
+    # return max area
     pass
 
 
